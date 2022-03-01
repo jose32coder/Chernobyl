212be3d62870b73c4b24a26777c81a30752ba808
José Manuel <81641564+jose32coder@users.noreply.github.com>
2022-02-28 20:48:53 -0400

    Delete assets/javascript directory

--- assets/javascript/main.js
+++
@@ -1,8 +0,0 @@
-//  ==================    MENU    ==================
-
-window.addEventListener('scroll', () =>{
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0)
-    let logo = document.querySelector('.logo');
-    logo.classList.toggle('logo2', window.scrollY > 0)
-})
